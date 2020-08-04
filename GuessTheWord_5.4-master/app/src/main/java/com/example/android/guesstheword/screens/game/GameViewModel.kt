92f1ecb7d0647df869dc0aa66e4cdeb8589e45ef
Pongsakorn <pongsakorn.4.7.10pt@gmail.com>
2020-08-04 23:06:38 +0700

    Initial commit

---
+++ GuessTheWord_5.4-master/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,148 @@
+package com.example.android.guesstheword.screens.game
+
+import android.os.CountDownTimer
+import android.text.format.DateUtils
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+    private val timer: CountDownTimer
+
+    companion object {
+
+        // Time when the game is over
+        private const val DONE = 0L
+
+        // Countdown time interval
+        private const val ONE_SECOND = 1000L
+
+        // Total time for the game
+        private const val COUNTDOWN_TIME = 60000L
+    }
+
+    // The current word
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
+    // The current scores
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
+    // Countdown time
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    // The String version of the current time
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
+
+    // Event which triggers the end of the game
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish: LiveData<Boolean>
+        get() = _eventGameFinish
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    init {
+
+        _word.value = ""
+        _score.value = 0
+
+        resetList()
+        nextWord()
+        Log.i("GameViewModel", "GameViewModel created!")
+
+        // Creates a timer which triggers the end of the game when it finishes
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished / ONE_SECOND
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+            }
+        }
+
+        timer.start()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        // Shuffle the word list, if the list is empty
+        if (wordList.isEmpty()) {
+            resetList()
+        } else {
+            // Remove a word from the list
+            _word.value = wordList.removeAt(0)
+        }
+    }
+    /** Methods for buttons presses **/
+    fun onSkip() {
+        if (!wordList.isEmpty()) {
+            _score.value = (score.value)?.minus(1)
+        }
+        nextWord()
+    }
+
+    fun onCorrect() {
+        if (!wordList.isEmpty()) {
+            _score.value = (score.value)?.plus(1)
+        }
+        nextWord()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer.cancel()
+    }
+
+    fun onGameFinishComplete() {
+        _eventGameFinish.value = false
+    }
+
+    fun onGameFinish() {
+        _eventGameFinish.value = true
+    }
+}
